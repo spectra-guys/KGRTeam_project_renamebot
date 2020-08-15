6e16f99998acb5b596cead8dde287a74cdd0362c
spectra-guys <69073757+spectra-guys@users.noreply.github.com>
2020-08-15 18:18:32 +0530

    Update By Me

---
+++ README.md
@@ -0,0 +1,23 @@
+## Rename Bot - The Bot Gram
+
+An Open Source Telegram Rename Bot & Don't create one because there are alot of it.
+
+---
+👺 Rename any Telegram Files with Permanent Thumbnail Support
+
+
+### Re-Action
+
+
+### Don't Play with Heroku they won't like this
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/prgofficial/RenameBot-PermTB)
+
+For Doubt's Msg me [BG_OFFICIAL](https://telegram.dog/BG_OFFICIAL)
+
+## Credits, and Thanks to Beloved Developers & You have done great work but I managed to recode it ;
+
+* SpEcHlDe
+* Dan Tès 
+* Yoily
+* Anand
+* ViruZz
---
+++ translation.py
@@ -0,0 +1,36 @@
+class Translation(object):
+    START_TEXT = """ 
+♥️<b>Created By:</b>@KGRockersmylife
+
+<i>Thanks For Using Our Bot..<i>"""
+
+    RENAME_403_ERR = "Don't rename the files of Master"
+    ABS_TEXT = "Something you are doing but I don't get you"
+    UPGRADE_TEXT = "Contact Creator @KGRockersmylife "
+    DOWNLOAD_START = "Getting files to my Server"
+    UPLOAD_START = "Senting files to you"
+    RCHD_TG_API_LIMIT = "Got Your Files in {} seconds.\nFound the File Size: {}\nSorry. But Make Sure The File is lesser than this😨😰"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "** Follow Us @The_Bot_Gram **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Got Your File in our Server in {} seconds.\nSent to you in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "You are Not Authorised to do This Contact @BG_OFFICIAL"
+    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Saved... Created By @KGRockersmylife"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Clearning the Thumbnail as your wish 😋"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Deleteing.....😨😰"
+    SAVED_RECVD_DOC_FILE = "Got Your Files & Renaming with please wait...🤩👍."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "We don't get any thumbnail from you😰"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """
+<b>Hey,Myself I'm a File Rename bot</b> 😇
+    
+Step 1: Send me any Telegram File 😋 or 
+Just forward it here 😜.
+Step 2: Reply to that message 
+with /rename newname .extension. 
+EG : /rename MovieName . mp4
+
+Bot Created By:
+👑 @KGRockersmylife 👑
+    """
+
